f1f4d86633d57557709d1dd50549578e631b28d7
Anthony Bioseh <61956668+buchz360@users.noreply.github.com>
2026-09-16 19:21:08 +0100

    Initialize Supabase client in supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL =
+  "https://aszdwaivuaiwzslgttgr.supabase.co";
+
+const SUPABASE_PUBLIC_KEY =
+  "sb_publishable_LCNOHaj_NxWaSZwzkjU_7w_c086mDeS";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLIC_KEY
+);
